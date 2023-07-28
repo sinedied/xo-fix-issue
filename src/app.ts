11b762dfbfc91ab180d78bd5a3d9a86f212be986
sinedied <noda@free.fr>
2023-07-28 10:22:12 +0200

    update

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
-import {join} from 'node:path';
-import AutoLoad, {type AutoloadPluginOptions} from '@fastify/autoload';
-import {type FastifyPluginAsync} from 'fastify';
+import path, { join } from 'node:path';
+import { fileURLToPath } from 'node:url';
+import AutoLoad, { type AutoloadPluginOptions } from '@fastify/autoload';
+import { type FastifyPluginAsync } from 'fastify';
 
 export type AppOptions = {
   // Place your custom options for app below here.
@@ -9,9 +10,12 @@ export type AppOptions = {
 // Pass --options via CLI arguments in command to enable these options.
 const options: AppOptions = {};
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const app: FastifyPluginAsync<AppOptions> = async (
   fastify,
-  options_,
+  options_
 ): Promise<void> => {
   // Place here your custom code!
 
@@ -22,16 +26,16 @@ const app: FastifyPluginAsync<AppOptions> = async (
   // through your application
   void fastify.register(AutoLoad, {
     dir: join(__dirname, 'plugins'),
-    options: options_,
+    options: options_
   });
 
   // This loads all plugins defined in routes
   // define your routes in one of these
   void fastify.register(AutoLoad, {
     dir: join(__dirname, 'routes'),
-    options: options_,
+    options: options_
   });
 };
 
 export default app;
-export {app, options};
+export { app, options };
