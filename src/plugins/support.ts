ee6e563ec923d4a1406810033a55b30ba3dec061
sinedied <noda@free.fr>
2023-07-28 10:18:06 +0200

    update

--- src/plugins/support.ts
+++ src/plugins/support.ts
@@ -1,14 +1,24 @@
+import process from 'node:process';
+import path from 'node:path';
 import fp from 'fastify-plugin';
+
+export interface SomeSupport {
+  test: string;
+}
 
 // The use of fastify-plugin is required to be able
 // to export the decorators to the outer scope
 export default fp(async (fastify, options) => {
-  fastify.decorate('someSupport', () => 'hugs');
+  console.log(path.resolve(process.cwd(), '../../.env'));
+
+  fastify.decorate('someSupport', {
+    test: 'test',
+  });
 });
 
 // When using .decorate you have to specify added properties for Typescript
 declare module 'fastify' {
   export interface FastifyInstance {
-    someSupport(): string;
+    someSupport: SomeSupport;
   }
 }
